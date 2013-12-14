f79c625ab3ed3c72c697a9f3edd842a680d6b5ab
OpenShift guest <borisb0ri5@gmail.com>
2013-12-13 23:13:34 -0500

    Move the go samples to samples and outside of src now that we've modified the go script to copy them to src.

--- samples/fortytwo_test.go
+++ samples/fortytwo_test.go
@@ -4,6 +4,6 @@ import "testing"
 
 func TestFortytwo(t *testing.T) {
 	if x := fortytwo(); x != 42 {
-		t.Errorf("Expected 42 but was %v", x);	
+		t.Errorf("Expected 42 but was %v", x);        
 	}
 }
