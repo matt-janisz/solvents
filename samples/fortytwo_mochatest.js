128a219aeab41418a55594405b0f607c15490996
OpenShift guest <borisb0ri5@gmail.com>
2013-12-16 01:14:15 -0500

    Complete the js build script for mochatests.

---
+++ samples/fortytwo_mochatest.js
@@ -0,0 +1,10 @@
+var assert = require('assert');
+var fortytwo = require('./fortytwo');
+
+describe('fortytwo', function() {
+  describe('#fortytwo()', function() {
+    it('should return 42', function() {
+	assert.equal(42, fortytwo.fortytwo());
+    })
+  })
+})
