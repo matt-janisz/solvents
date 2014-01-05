696add2d84ed625ba5d8093adacbb7f113be856f
b0ri5 <borisb0ri5@gmail.com>
2014-01-05 09:18:29 -0500

    Add a brute force js solution for project euler 1.

---
+++ projecteuler/0001/multiples_of_3_and_5.js
@@ -0,0 +1,11 @@
+var sum_multiples_of_3_and_5_brute_force = function(n) {
+  var sum = 0;
+  for (int i = 0; i < below; i++) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+
+exports.sum_multiples_of_3_and_5_brute_force = sum_multiples_of_3_and_5_brute_force;
