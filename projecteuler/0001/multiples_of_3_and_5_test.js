a79d18588395f48499499e66defc52f1ef578e20
b0ri5 <borisb0ri5@gmail.com>
2014-01-05 09:25:33 -0500

    Add a test for the brute force mutiples_of_3_and_5

---
+++ projecteuler/0001/multiples_of_3_and_5_test.js
@@ -0,0 +1,19 @@
+var assert = require('assert');
+var multiples_of_3_and_5 = require('./multiples_of_3_and_5');
+
+describe('multiples_of_3_and_5', function() {
+  describe('#multiples_of_3_and_5_brute_force()', function() {
+    it('should be exclusive', function() {
+      assert.equal(0, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(3));
+      assert.equal(3, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(4));
+      assert.equal(3, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(5));
+      assert.equal(8, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(6));
+    });
+    it('should solve small example', function() {
+      assert.equal(23, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(10));
+    });
+    it('should solve large problem', function() {
+      assert.equal(233168, multiples_of_3_and_5.multiples_of_3_and_5_brute_force(1000));
+    });
+  });
+});
