e999f5fb36f5b5dd54335e9cf1aa6d63f854f9ad
b0ri5 <borisb0ri5@gmail.com>
2014-01-05 11:42:13 -0500

    s/below/n

--- projecteuler/0001/multiples_of_3_and_5.js
+++ projecteuler/0001/multiples_of_3_and_5.js
@@ -1,6 +1,6 @@
 var sum_multiples_of_3_and_5_brute_force = function(n) {
   var sum = 0;
-  for (var i = 0; i < below; i++) {
+  for (var i = 0; i < n; i++) {
     if (i % 3 == 0 || i % 5 == 0) {
       sum += i;
     }
