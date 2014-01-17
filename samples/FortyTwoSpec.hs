f83bcd9412f657d85e9dcf8b867355d61645dad9
Greg Tener <borisb0ri5@gmail.com>
2014-01-16 23:00:01 -0800

    Enforce linting for haskell.

--- samples/FortyTwoSpec.hs
+++ samples/FortyTwoSpec.hs
@@ -7,7 +7,7 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec = do
-  describe "fortytwo" $ do
-    it "should return 42" $ do
+spec =
+  describe "fortytwo" $
+    it "should return 42" $
       fortytwo `shouldBe` 42
